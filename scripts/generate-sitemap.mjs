9ddbae3b99dcd8ac9537904e9bb67c3f9b75c6e3
Saqin Noor <saqin@saqinnoor.dpdns.org>
2026-06-25 06:12:05 +0600

    feat: add sitemap generation on build

---
+++ scripts/generate-sitemap.mjs
@@ -0,0 +1,20 @@
+import { writeFileSync } from 'fs';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const SITE_URL = 'https://saqinnoor.dpdns.org';
+const outDir = resolve(__dirname, '../dist');
+const today = new Date().toISOString().split('T')[0];
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${SITE_URL}/</loc>
+    <lastmod>${today}</lastmod>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+writeFileSync(resolve(outDir, 'sitemap.xml'), sitemap);
+console.log('✓ sitemap.xml generated');
